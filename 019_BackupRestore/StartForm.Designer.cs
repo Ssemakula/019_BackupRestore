8d73c5ce2c27393f91239fac10abf9c1707c05f5
Ssemakula Mukiibi <Ssemakula@users.noreply.github.com>
2024-06-24 21:06:14 +0300

    Add project files.

---
+++ 019_BackupRestore/StartForm.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace _019_BackupRestore
+{
+    partial class StartForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backupButton = new System.Windows.Forms.Button();
+            this.restoreButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // backupButton
+            // 
+            this.backupButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.backupButton.Location = new System.Drawing.Point(75, 91);
+            this.backupButton.Name = "backupButton";
+            this.backupButton.Size = new System.Drawing.Size(90, 89);
+            this.backupButton.TabIndex = 0;
+            this.backupButton.Text = "Backup!";
+            this.backupButton.UseVisualStyleBackColor = true;
+            this.backupButton.Click += new System.EventHandler(this.BackupButton_Click);
+            // 
+            // restoreButton
+            // 
+            this.restoreButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.restoreButton.Location = new System.Drawing.Point(190, 91);
+            this.restoreButton.Name = "restoreButton";
+            this.restoreButton.Size = new System.Drawing.Size(90, 89);
+            this.restoreButton.TabIndex = 1;
+            this.restoreButton.Text = "Restore!";
+            this.restoreButton.UseVisualStyleBackColor = true;
+            this.restoreButton.Click += new System.EventHandler(this.restoreButton_Click);
+            // 
+            // StartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 471);
+            this.Controls.Add(this.restoreButton);
+            this.Controls.Add(this.backupButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "StartForm";
+            this.Text = "Shop Pro";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button backupButton;
+        private System.Windows.Forms.Button restoreButton;
+    }
+}
+
---
+++ 019_BackupRestore/StartForm.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static NotifyMethods.NotifyMessage;
+
+namespace _019_BackupRestore
+{
+    public partial class StartForm : Form
+    {
+        public StartForm()
+        {
+            InitializeComponent();
+        }
+
+        void BackupDatabase()
+        {
+            System.Data.SqlClient.SqlCommand command = new System.Data.SqlClient.SqlCommand();
+            System.Data.SqlClient.SqlConnection connection = new System.Data.SqlClient.SqlConnection();
+            string qryText;
+
+            connection.ConnectionString = _019_BackupRestore.Properties.Settings.Default.LoginLessonConnectionString;
+            command.Connection = connection;
+            qryText = @"BACKUP DATABASE [phone_book_Lesson] TO  DISK = N'C:\DatabasesHold\Backups\phone_book_Lesson3.bak' 
+                    WITH NOFORMAT, INIT,  NAME = N'phone_book_Lesson-Full Database Backup', 
+                    SKIP, NOREWIND, NOUNLOAD,  STATS = 10";
+            try
+            {
+                command.CommandTimeout = 30; //This is the default so it is a bit unnecessary other than an example
+                connection.Open();
+                command.CommandText = qryText;
+                command.ExecuteNonQuery();
+                connection.Close();
+                Notify("Backup successful");
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        void RestoreDatabase()
+        {
+            System.Data.SqlClient.SqlCommand command = new System.Data.SqlClient.SqlCommand();
+            System.Data.SqlClient.SqlConnection connection = new System.Data.SqlClient.SqlConnection();
+            string qryText;
+
+            connection.ConnectionString = _019_BackupRestore.Properties.Settings.Default.LoginLessonConnectionString;
+            command.Connection = connection;
+            // Think of setting it to choose file
+            qryText = @"USE [master]
+                        ALTER DATABASE [phone_book_Lesson] SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+                        RESTORE DATABASE [phone_book_Lesson] 
+                        	FROM  DISK = N'C:\DatabasesHold\Backups\phone_book_Lesson3.bak' 
+                        	WITH  FILE = 1,  NOUNLOAD,  REPLACE,  STATS = 5
+                        ALTER DATABASE [phone_book_Lesson] SET MULTI_USER";
+            try
+            {
+                command.CommandTimeout = 30; //This is the default so it is a bit unnecessary other than an example
+                connection.Open();
+                command.CommandText = qryText;
+                command.ExecuteNonQuery();
+                connection.Close();
+                Notify("Restore successful");
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void BackupButton_Click(object sender, EventArgs e)
+        {
+            BackupDatabase();
+        }
+
+        private void restoreButton_Click(object sender, EventArgs e)
+        {
+            RestoreDatabase();
+        }
+    }
+}
